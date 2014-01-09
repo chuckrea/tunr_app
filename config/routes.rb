1c5e8f12b46ed0f27016f4ac3082a050f2c90f27
chuckrea <chuckr523@mac.com>
2014-01-09 15:23:16 -0500

    tom morello can be in RAGE

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 TunrApp::Application.routes.draw do
 
   resources :musicians
+  resources :bands
+  resources :band_memberships
   
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140109155124) do
+ActiveRecord::Schema.define(:version => 20140109200502) do
+
+  create_table "band_memberships", :force => true do |t|
+    t.integer  "band_id"
+    t.integer  "musician_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "bands", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "musicians", :force => true do |t|
     t.string   "name"
